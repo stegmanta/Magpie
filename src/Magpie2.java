190f26e4df621ab5ef76d68fc0875a481ee0ba28
Tayler Stegman <Tae@myfreeapps.com>
2019-11-19 12:53:01 -0700

    tried to add magpie runner 3 to 2

--- src/Magpie2.java
+++ src/Magpie2.java
@@ -49,8 +49,7 @@ public class Magpie2 {
 					response = "What an awesome fella";
 					}else if(trimmedStatement.equals("")) {
 						response = "Say something please";
-
-					
+							}else if(statement.indexOf("Do you like candy") {
 							}else{
 								response = getRandomResponse();
 								}	return response;
@@ -79,6 +78,43 @@ public class Magpie2 {
 
 		return response;
 	}
+	private int findKeyword(String statement, String goal, int startPos) {
+		String phrase = statement.trim().toLowerCase();
+		goal = goal.toLowerCase();
+
+		// The only change to incorporate the startPos is in
+		// the line below
+		int psn = phrase.indexOf(goal, startPos);
+
+		// Refinement--make sure the goal isn't part of a
+		// word
+		while (psn >= 0) {
+			// Find the string of length 1 before and after
+			// the word
+			String before = " ", after = " ";
+			if (psn > 0) {
+				before = phrase.substring(psn - 1, psn);
+			}
+			if (psn + goal.length() < phrase.length()) {
+				after = phrase.substring(psn + goal.length(),
+						psn + goal.length() + 1);
+			}
+
+			// If before and after aren't letters, we've
+			// found the word
+			if (((before.compareTo("a") < 0) || (before.compareTo("z") > 0)) // before
+																				// is
+																				// not
+																				// a
+																				// letter
+					&& ((after.compareTo("a") < 0) || (after.compareTo("z") > 0))) {
+				return psn;
+			}
+			private int findKeyword(String statement, String goal) {
+				return findKeyword(statement, goal, 0);
+			}
 
 }
+
+
 	
